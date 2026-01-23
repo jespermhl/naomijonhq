fd7d0fb1cec4b55a0fcffb8fdd8c963040d12e6a
jespermhl <kinsej@posteo.de>
2026-01-23 14:57:08 +0100

    Create error message on newsletter page

--- app/newsletter/page.tsx
+++ app/newsletter/page.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link";
+
 export default function NewsletterPage() {
     return (
         <main>
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontFamily: 'sans-serif' }}>
-                <h1>Newsletter</h1>
+                <div style={{ textAlign: 'center' }}>
+                    <h1>Our newsletter form ghosted us 👻. You can still drop your email at <Link href="mailto:info@naomijonhq.com">info@naomijonhq.com</Link>!</h1>
+                    <p>Try refreshing the page if the issue persists.</p>
+                </div>
             </div>
         </main>
     );
