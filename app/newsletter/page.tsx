97399e7d42d2ed8929225d9eca14240a7854be82
jespermhl <kinsej@posteo.de>
2026-01-23 16:17:25 +0100

    docs: Add browser opening tip to newsletter page

--- app/newsletter/page.tsx
+++ app/newsletter/page.tsx
@@ -72,7 +72,7 @@ export default function NewsletterPage() {
                     marginBottom: '40px',
                     fontWeight: '600'
                 }}>
-                    If the form is not showing up, try refreshing the page, waiting for a few seconds, or subscribing later. You can also say hi at:<br />
+                    If the form is not showing up, try opening this page in your browser (instead of inside Instagram or TikTok) or waiting for a few seconds. You can also say hi at:<br />
                     <Link
                         href="mailto:info@naomijonhq.com"
                         style={{
