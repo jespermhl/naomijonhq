52c42d188e930e8bc20a23d43f13c1fb07cce0af
Jesper <kinsej@posteo.de>
2026-06-07 16:22:07 +0200

    feat: add TSDoc plugin for linting and install TypeDoc for documentation generation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,20 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import tsdoc from "eslint-plugin-tsdoc";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  {
+    plugins: {
+      tsdoc: tsdoc,
+    },
+    rules: {
+      "tsdoc/syntax": "warn",
+    },
+  },
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
